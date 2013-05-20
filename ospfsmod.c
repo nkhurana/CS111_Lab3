646135da7d97378b18807f10e9355d19f715817b
Yifeng Evan Shi <eshi@ucla.edu>
2013-05-20 02:18:06 -0700

    Commit readdir

--- ospfsmod.c
+++ ospfsmod.c
@@ -459,8 +459,11 @@ static int ospfs_dir_readdir(struct file *filp, void *dirent, filldir_t filldir)
 		 * the loop.  For now we do this all the time.
 		 *
 		 * EXERCISE: Your code here */
-		r = 1;		/* Fix me! */
-		break;		/* Fix me! */
+		if (f_pos * OSPFS_DIRENTRY_SIZE > dir_oi->oi_size)
+		{
+			r = 1;
+			break;
+		}
 
 		/* Get a pointer to the next entry (od) in the directory.
 		 * The file system interprets the contents of a
@@ -483,6 +486,26 @@ static int ospfs_dir_readdir(struct file *filp, void *dirent, filldir_t filldir)
 		 */
 
 		/* EXERCISE: Your code here */
+		od = ospfs_inode_data(dir_oi, f_pos * OSPFS_DIRENTRY_SIZE);
+		if (od->od_ino > 0)
+		{
+			entry_oi = ospfs_inode(od->od_ino);
+			uint32_t ftype;
+			if (entry_oi->oi_ftype == OSPFS_FTYPE_REG)
+				ftype = DT_REG;
+			else if (entry_oi->oi_ftype == OSPFS_FTYPE_DIR)
+				ftype = DT_DIR;
+			else if (entry_oi->oi_ftype == OSPFS_FTYPE_SYMLINK)
+				ftype = DT_LNK;
+			
+			ok_so_far = filldir(dirent, od->od_name, strlen(od->od_name), f_pos, od->od_ino, ftype);
+			if (ok_so_far >= 0)
+				f_pos++;
+			else
+				break;
+		}
+		else
+			f_pos++;
 	}
 
 	// Save the file position and return!
@@ -1217,10 +1240,18 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 	// Support files opened with the O_APPEND flag.  To detect O_APPEND,
 	// use struct file's f_flags field and the O_APPEND bit.
 	/* EXERCISE: Your code here */
-
+	uint32_t write_pos = 0;
+	if (filp->f_flags & O_APPEND != 0)
+		write_pos = f_pos;
+	
 	// If the user is writing past the end of the file, change the file's
 	// size to accomodate the request.  (Use change_size().)
 	/* EXERCISE: Your code here */
+	if (write_pos != 0)
+		change_size(oi, oi->oi_size + count);
+	else if (count > oi->oi_size)
+		change_size(oi, count);
+		
 
 	// Copy data block by block
 	while (amount < count && retval >= 0) {
